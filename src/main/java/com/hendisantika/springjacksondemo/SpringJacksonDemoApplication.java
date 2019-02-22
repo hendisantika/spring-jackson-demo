319bc2d958f7e805c61178938898c052ba51f0f3
Hendi Santika <hendi.santika@allianz.co.id>
2019-02-22 18:26:50 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springjacksondemo/SpringJacksonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springjacksondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJacksonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJacksonDemoApplication.class, args);
+    }
+
+}
